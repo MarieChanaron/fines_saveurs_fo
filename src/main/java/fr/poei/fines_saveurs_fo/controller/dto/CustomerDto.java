e17002874eb18c065567f6b90da2c6ae21bc8d4a
Amina Cherni <cherniamina21@gmail.com>
2023-05-21 19:57:02 +0200

    afficher les détails du profile du client

--- src/main/java/fr/poei/fines_saveurs_fo/controller/dto/CustomerDto.java
+++ src/main/java/fr/poei/fines_saveurs_fo/controller/dto/CustomerDto.java
@@ -1,4 +1,19 @@
 package fr.poei.fines_saveurs_fo.controller.dto;
 
+
+import fr.poei.fines_saveurs_fo.entity.Order;
+import lombok.Data;
+
+
+import java.util.List;
+
+@Data
 public class CustomerDto {
+    private int id;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private List<Order> orders;
+
 }
