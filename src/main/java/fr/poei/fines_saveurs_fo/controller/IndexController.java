47040d67502541bd50a4bc040e0caa26fadd9b0d
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-24 00:52:53 +0200

    add data to the cart saved in the redis session

--- src/main/java/fr/poei/fines_saveurs_fo/controller/IndexController.java
+++ src/main/java/fr/poei/fines_saveurs_fo/controller/IndexController.java
@@ -1,5 +1,6 @@
 package fr.poei.fines_saveurs_fo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
     @GetMapping
     public String index () {
         return "redirect:/products";
