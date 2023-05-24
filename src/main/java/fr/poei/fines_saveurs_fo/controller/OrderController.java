86e0d8210f2f92a6e6b79d887d348be0e0ae26be
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-24 19:47:24 +0200

    display the cartDto in the console in Order page

--- src/main/java/fr/poei/fines_saveurs_fo/controller/OrderController.java
+++ src/main/java/fr/poei/fines_saveurs_fo/controller/OrderController.java
@@ -1,20 +1,28 @@
 package fr.poei.fines_saveurs_fo.controller;
 
+import fr.poei.fines_saveurs_fo.controller.dto.CartDto;
+import fr.poei.fines_saveurs_fo.controller.dto.MapStructMapper;
 import fr.poei.fines_saveurs_fo.entity.Cart;
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/order")
 public class OrderController {
+
+    final MapStructMapper mapStructMapper;
 
     @GetMapping
     public String order(HttpSession session, Model model) {
         session.removeAttribute("redirect");
         Cart cart = (Cart) session.getAttribute("cart");
+        CartDto cartDto = mapStructMapper.toDto(cart);
+        System.out.println(cartDto);
         if (cart.getCustomer() == null) { // If the customer is not logged in
             session.setAttribute("redirect", "order");
             return "redirect:/login";
