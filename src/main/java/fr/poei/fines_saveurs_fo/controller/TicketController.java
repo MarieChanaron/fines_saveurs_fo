871283b387bf4d8ad1d939df06ce896a9202a7b4
Truong <tvn.terence@gmail.com>
2023-05-28 18:32:05 +0200

    add auto email on tickets

--- src/main/java/fr/poei/fines_saveurs_fo/controller/TicketController.java
+++ src/main/java/fr/poei/fines_saveurs_fo/controller/TicketController.java
@@ -1,8 +1,5 @@
 package fr.poei.fines_saveurs_fo.controller;
 
-import fr.poei.fines_saveurs_fo.entity.Ticket;
-import jakarta.inject.Scope;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -10,7 +7,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -19,11 +15,10 @@ public class TicketController {
     @GetMapping("/ticket")
     public String displayTicketForm(Model model, HttpSession session) {
 
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String userEmail = authentication.getName();
-//
-//        System.out.println(userEmail);
-//        model.addAttribute("userEmail", userEmail);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
+
+        model.addAttribute("userEmail", userEmail);
 
         return "ticket";
     }
