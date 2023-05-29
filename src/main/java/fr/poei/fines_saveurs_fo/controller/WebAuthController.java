b38fcd91f53fc6a056118022170d9de921f599a5
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-29 22:39:39 +0200

    clean controllers

--- src/main/java/fr/poei/fines_saveurs_fo/controller/WebAuthController.java
+++ src/main/java/fr/poei/fines_saveurs_fo/controller/WebAuthController.java
@@ -3,14 +3,12 @@ package fr.poei.fines_saveurs_fo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/erreur")
-public class TestController {
+public class WebAuthController {
 
-    @GetMapping
-    public String erreur(Model model) {
-        return "404";
+    @GetMapping("/login")
+    public String loginForm (Model model) {
+        return "login";
     }
 }
